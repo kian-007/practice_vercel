30ef7bd56515c5d8c2267d551baea730c9d3f9b1
kian-007 <king.kian008@gmail.com>
2026-09-20 04:44:56 +0330

    set cookies path

--- practice-backend/routes/auth.js
+++ practice-backend/routes/auth.js
@@ -80,12 +80,14 @@ router.post(
         httpOnly: true,
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
+        path: "/",
         maxAge: 15 * 60 * 1000,
       });
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
+        path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
       res.json({ success: true, message: "successful login :D" });
@@ -376,12 +378,14 @@ router.post(
       httpOnly: true,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
+      path: "/",
       maxAge: 15 * 60 * 1000,
     });
     res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.json({ success: true });
